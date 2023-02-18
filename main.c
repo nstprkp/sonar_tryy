f28ad33fac642882b06edd1ba478469765584d07
nstprkp <113131930+nstprkp@users.noreply.github.com>
2023-02-18 12:44:43 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,17 +2,13 @@
 
 int main()
 {
-    int x=1,y=1;
-    int Solution=0;
-    while (x<=9 && Solution==0) {
-        y=1;
-        while (y<=9 && Solution==0) {
-            if (x+y==11 && x*10+y+27==y*10+x) { Solution=1;}
-            else {y++;}
-        }
-        if (Solution==0) {x++;}
-    }
-    if (x>9) { printf("We don't have a solution.\n");}
-    else {printf("Your solution is: x=%i, y=%i.\n", x, y);}
+    float x1,x2,y1,y2;
+    printf("Coordinates of the first point: ");
+    scanf("%f %f",&x1, &y1);
+    printf("Coordinates of the second point: "); scanf("%f %f",&x2, &y2);
+
+    float dist=0;
+    dist=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+    printf("Distance = %.2f", dist);
     return 0;
 }
